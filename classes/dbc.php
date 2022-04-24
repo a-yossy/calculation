0b5c8f2a4e6863a24e9cdd84f6335d6d41c4f601
atsuya02 <atsuya0930@yahoo.co.jp>
2022-04-24 17:56:03 +0900

    feat: データベースに接続するため、Dbcクラス作成

---
+++ classes/dbc.php
@@ -0,0 +1,26 @@
+<?php
+  require_once('../config/env.php');
+
+  class Dbc{
+    protected $tableName;
+
+    protected function dbConnect() {
+      $host = DB_HOST;
+      $dbname = DB_NAME;
+      $user = DB_USER;
+      $pass = DB_PASS;
+      $dsh = "mysql:host=$host;dbname=$dbname;charset=utf8";
+
+      try {
+        $dbh = new PDO($dsh, $user, $pass, [
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
+        return $dbh;
+      } catch (PDOException $e) {
+        header('Content-Type: text/plain; charset=UTF-8', true, 500);
+        exit($e->getMessage());
+      }
+    }
+  }
+?>
